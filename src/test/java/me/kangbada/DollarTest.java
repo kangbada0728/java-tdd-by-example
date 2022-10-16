f469bb48811fd0aab0aacc095a3f1c7f0628fb99
bada.kang <kangbada0728@gmail.com>
2022-10-16 20:40:01 +0900

    2장. 타락한 객체

--- src/test/java/me/kangbada/DollarTest.java
+++ src/test/java/me/kangbada/DollarTest.java
@@ -8,7 +8,9 @@ public class DollarTest {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
+        Dollar product = five.times(2);
         assertEquals(10, five.amount);
+        product = five.times(3);
+        assertEquals(15, five.amount);
     }
 }
