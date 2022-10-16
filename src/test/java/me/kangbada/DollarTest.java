fbd5af5382b524545a44275000de0c7a80dafcbf
bada.kang <kangbada0728@gmail.com>
2022-10-16 20:05:08 +0900

    1장. 다중 통화를 지원하는 Money 객체

---
+++ src/test/java/me/kangbada/DollarTest.java
@@ -0,0 +1,14 @@
+package me.kangbada;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DollarTest {
+    @Test
+    void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
