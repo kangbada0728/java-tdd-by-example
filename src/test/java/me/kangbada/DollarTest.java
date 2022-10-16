2d47aaad78a2371f1b7bc0536f075cda7b9acdfe
bada.kang <kangbada0728@gmail.com>
2022-10-16 20:46:29 +0900

    3장. 모두를 위한 평등

--- src/test/java/me/kangbada/DollarTest.java
+++ src/test/java/me/kangbada/DollarTest.java
@@ -2,7 +2,7 @@ package me.kangbada;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DollarTest {
     @Test
@@ -12,5 +12,11 @@ public class DollarTest {
         assertEquals(10, five.amount);
         product = five.times(3);
         assertEquals(15, five.amount);
+    }
+
+    @Test
+    void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
